59b8debb7f9e0c91b524b7e84209d5e68db5801f
Henry <spike.1@live.co.uk>
2017-12-19 23:50:15 +0000

    Fixed some tab errors

--- src/BookShelf.js
+++ src/BookShelf.js
@@ -8,21 +8,19 @@ import BookThumbnail from './BookThumbnail'
 class BookShelf extends React.Component {
   render() {
     return (
-      <div>
-        <div className="bookshelf">
-          <h2 className="bookshelf-title">{this.props.title}</h2>
-          <div className="bookshelf-books">
-            <ol className="books-grid">          
-              {this.props.books.map((book) => (
-      		    <li key={book.id}>
-                  <BookThumbnail
-      			    book={book}
-      				changeShelves={this.props.changeShelves}
-      			  />
-      			</li>
-              ))}
-            </ol>
-          </div>
+      <div className="bookshelf">
+        <h2 className="bookshelf-title">{this.props.title}</h2>
+        <div className="bookshelf-books">
+          <ol className="books-grid">
+            {this.props.books.map((book) => (
+              <li key={book.id}>
+                <BookThumbnail
+                  book={book}
+                  changeShelves={this.props.changeShelves}
+                />
+              </li>
+            ))}
+          </ol>
         </div>
       </div>
     )
--- src/Home.js
+++ src/Home.js
@@ -10,49 +10,48 @@ import BookShelf from './BookShelf'
 import sortBy from 'sort-by';
 
 class Home extends React.Component {
-  render() {    
+  render() {
     const books = this.props.books.sort(sortBy('title'))
-    
+
     const currentBooks = books
       .filter((book) => book.shelf === "currentlyReading")
-    
+
     const wantToReadBooks = books
       .filter((book) => book.shelf === "wantToRead")
-    
+
     const readBooks = books
       .filter((book) => book.shelf === "read")
-    
+
     const noneBooks = books
       .filter((book) => book.shelf === "none")
-    
+
     return (
       <div className="list-books">
-      
         <div className="list-books-title">
           <h1>MyReads</h1>
         </div>
-      
+
         <div className="list-books-content">
           <BookShelf
-      		title="Currently Reading"
-      		books={currentBooks}
-      		changeShelves={this.props.changeShelves}
-      	  />
+            title="Currently Reading"
+            books={currentBooks}
+            changeShelves={this.props.changeShelves}
+          />
           <BookShelf
-      		title="Want To Read"
-      		books={wantToReadBooks}
+            title="Want To Read"
+            books={wantToReadBooks}
       	  	changeShelves={this.props.changeShelves}
-      	  />
+          />
           <BookShelf
-      		title="Read"
-      		books={readBooks}
-      		changeShelves={this.props.changeShelves}
-      	  />
-      	  <BookShelf
-      		title="None" 
-      		books={noneBooks}
-      	    changeShelves={this.props.changeShelves}
-      	  />
+            title="Read"
+            books={readBooks}
+            changeShelves={this.props.changeShelves}
+          />
+          <BookShelf
+            title="None"
+            books={noneBooks}
+            changeShelves={this.props.changeShelves}
+          />
         </div>
 
         <div className="open-search">
