feae07b4ce2faef04082776cddd5766158ab628c
Henry <spike.1@live.co.uk>
2017-12-20 16:36:12 +0000

    Change home to use array of shelf objects

--- src/Home.js
+++ src/Home.js
@@ -10,50 +10,40 @@ import BookShelf from './BookShelf'
 import sortBy from 'sort-by';
 
 class Home extends React.Component {
-  render() {
+  render() {    
     const books = this.props.books.sort(sortBy('title'))
-
-    const currentBooks = books
-      .filter((book) => book.shelf === "currentlyReading")
-
-    const wantToReadBooks = books
-      .filter((book) => book.shelf === "wantToRead")
-
-    const readBooks = books
-      .filter((book) => book.shelf === "read")
-
-    const noneBooks = books
-      .filter((book) => book.shelf === "none")
-
+    const shelves = [
+       {
+      	shelf: "currentlyReading",
+      	title: "Currently Reading"
+      }, {
+        shelf: "wantToRead",
+      	title: "Want to read"
+      }, {
+        shelf: "read",
+        title: "Read"
+      }, {
+        shelf: "none",
+        title: "None"
+      }] 
     return (
       <div className="list-books">
+      
         <div className="list-books-title">
           <h1>MyReads</h1>
         </div>
-
+      
         <div className="list-books-content">
-          <BookShelf
-            title="Currently Reading"
-            books={currentBooks}
-            changeShelves={this.props.changeShelves}
-          />
-          <BookShelf
-            title="Want To Read"
-            books={wantToReadBooks}
-      	  	changeShelves={this.props.changeShelves}
-          />
-          <BookShelf
-            title="Read"
-            books={readBooks}
-            changeShelves={this.props.changeShelves}
-          />
-          <BookShelf
-            title="None"
-            books={noneBooks}
-            changeShelves={this.props.changeShelves}
-          />
-        </div>
-
+      	  {shelves.map((shelf) => (
+      	    <BookShelf
+      		  key={shelf.shelf}
+      		  title={shelf.title}
+      		  books={books.filter((book) => book.shelf === shelf.shelf)}
+      		  changeShelves={this.props.changeShelves}
+      	    />
+          ))}
+		</div>
+  
         <div className="open-search">
           <Link to="/search/">Add a book</Link>
         </div>
