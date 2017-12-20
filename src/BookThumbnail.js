22dcabe9e05176ff75f34eb6e17881ac1e44cf53
Henry <spike.1@live.co.uk>
2017-12-20 16:36:46 +0000

    Remove shelf state from BookThumbnail

--- src/BookThumbnail.js
+++ src/BookThumbnail.js
@@ -1,32 +1,29 @@
 //Base
 import React from 'react'
+//Router
+import { Link } from 'react-router-dom'
 //Styles
 import './App.css'
 
 class BookThumbnail extends React.Component {
-  state = {
-    shelf: this.props.book.shelf
-  }
-
-  updateShelf = (book, shelf) => {
-    this.props.changeShelves(book, shelf)
-    this.setState({ shelf: shelf }) 
-  }
   render() {
     const book = this.props.book
     const img = "https"+book.imageLinks.smallThumbnail.substring(4)
+    const link = "/book/" + book.id + "/"
     
     return (
-      <div className="book">
+      <div className="book fadeIn">
         <div className="book-top">
-          <div
+          <Link
+    		to={link}
       		className="book-cover"
-      		style={{ width: 128, height: 193, backgroundImage: `url(${img})` }}>
-          </div>
+      		style={{ width: 128, height: 193, backgroundImage: `url(${img})` }}
+		  >
+          </Link>
           <div className="book-shelf-changer">
             <select
 			  defaultValue={book.shelf}
-			  onChange={(event) => this.updateShelf(book, event.target.value)}
+			  onChange={(event) => this.props.changeShelves(book, event.target.value)}
 			>
               <option disabled>Move to...</option>
               <option value="currentlyReading">Currently Reading</option>
@@ -36,8 +33,8 @@ class BookThumbnail extends React.Component {
             </select>
           </div>
         </div>
-        <div className="book-title">{book.title}</div>
-        <div className="book-authors">{book.authors}</div>
+        <div className="book-title" onClick={(event) => console.log(event)}>{book.title}</div>
+        <div className="book-authors" onClick={(event) => console.log(event)}>{book.authors}</div>
       </div>
     )
   }
