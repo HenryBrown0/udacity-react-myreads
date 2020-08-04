02819668e846364787ea1e844e6deab5ffd3fb1e
Henry Brown <hb317@kent.ac.uk>
2020-08-04 22:06:56 +0100

    Remove sort-by

--- src/BookShelf.js
+++ src/BookShelf.js
@@ -1,37 +1,35 @@
 //Base
-import React from 'react'
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
 //Styles
-import './App.css'
+import "./App.css";
 //Components
-import BookThumbnail from './BookThumbnail'
-import sortBy from 'sort-by';
+import BookThumbnail from "./BookThumbnail";
 
 const BookShelf = (props) => {
-  const { title, changeShelves, books } = props;
-  return (
-    <div className="bookshelf">
-      <h2 className="bookshelf-title">{title}</h2>
-      <div className="bookshelf-books">
-        <ol className="books-grid">
-          {books.sort(sortBy('title')).map((book) => (
-            <li key={book.id}>
-              <BookThumbnail
-                book={book}
-                changeShelves={changeShelves}
-              />
-            </li>
-          ))}
-        </ol>
-      </div>
-    </div>
-  )
-}
+	const { title, changeShelves, books } = props;
+	return (
+		<div className="bookshelf">
+			<h2 className="bookshelf-title">{title}</h2>
+			<div className="bookshelf-books">
+				<ol className="books-grid">
+					{books
+						.sort((a, b) => b.title - a.title)
+						.map((book) => (
+							<li key={book.id}>
+								<BookThumbnail book={book} changeShelves={changeShelves} />
+							</li>
+						))}
+				</ol>
+			</div>
+		</div>
+	);
+};
 
 BookShelf.propTypes = {
-  title: PropTypes.string.isRequired,
-  changeShelves: PropTypes.func.isRequired,
-  books: PropTypes.array.isRequired
-}
+	title: PropTypes.string.isRequired,
+	changeShelves: PropTypes.func.isRequired,
+	books: PropTypes.array.isRequired,
+};
 
-export default BookShelf
+export default BookShelf;
--- src/Home.js
+++ src/Home.js
@@ -1,49 +1,47 @@
 //Base
-import React from 'react'
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
 //Router
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
 //Lodash
-import lodash from 'lodash'
+import { camelCase } from "lodash";
 //Styles
-import './App.css'
+import "./App.css";
 //Components
-import BookShelf from './BookShelf'
-import sortBy from 'sort-by';
+import BookShelf from "./BookShelf";
 
 const Home = (props) => {
-  const books = props.books.sort(sortBy('title'))
-  const { changeShelves } = props
-  const shelves = [ "Currently Reading", "Want to read", "Read", "None" ]
-  
-  return (
-    <div className="list-books">
+	const books = props.books.sort((a, b) => b.title - a.title);
+	const { changeShelves } = props;
+	const shelves = ["Currently Reading", "Want to read", "Read", "None"];
 
-      <div className="list-books-title">
-        <h1>MyReads</h1>
-      </div>
+	return (
+		<div className="list-books">
+			<div className="list-books-title">
+				<h1>MyReads</h1>
+			</div>
 
-      <div className="list-books-content">
-        {shelves.map((shelf) => (
-          <BookShelf
-            key={shelf}
-            title={shelf}
-            books={books.filter((book) => book.shelf === lodash.camelCase([shelf]))}
-            changeShelves={changeShelves}
-          />
-        ))}
-      </div>
+			<div className="list-books-content">
+				{shelves.map((shelf) => (
+					<BookShelf
+						key={shelf}
+						title={shelf}
+						books={books.filter((book) => book.shelf === camelCase([shelf]))}
+						changeShelves={changeShelves}
+					/>
+				))}
+			</div>
 
-      <div className="open-search">
-        <Link to="/search/">Add a book</Link>
-      </div>
-    </div>
-  )
-}
+			<div className="open-search">
+				<Link to="/search/">Add a book</Link>
+			</div>
+		</div>
+	);
+};
 
 Home.propTypes = {
-  changeShelves: PropTypes.func.isRequired,
-  books: PropTypes.array.isRequired
-}
+	changeShelves: PropTypes.func.isRequired,
+	books: PropTypes.array.isRequired,
+};
 
-export default Home
+export default Home;
