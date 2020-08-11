ea9e76a40a588eb2152254c69193552e959f36ec
Henry Brown <me@henrybrown0.com>
2020-08-11 09:57:38 +0100

    Change search to use hooks

--- src/Search.js
+++ src/Search.js
@@ -1,5 +1,5 @@
 // Base
-import React from "react";
+import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 // Router
 import { Link } from "react-router-dom";
@@ -11,87 +11,100 @@ import BookShelf from "./BookShelf";
 // ServerAPI
 import * as BooksAPI from "./BooksAPI";
 
-class Search extends React.Component {
-	constructor() {
-		super();
-		this.state = {
-			showingBooks: [],
-			query: ""
-		};
-	}
+const Search = (props) => {
+	const { books, changeShelves } = props;
 
-	render() {
-		const { books, changeShelves } = this.props;
-		const { showingBooks, query } = this.state;
+	const [showingBooks, setShowingBooks] = useState([]);
+	const [query, setQuery] = useState("");
 
-		const mergeBooks = (results, searchQueryString) => {
-			const match = new RegExp(escapeRegExp(searchQueryString), "i");
-			// find local results
-			const localResults = books.filter(
-				(b) => match.test(b.title) || match.test(b.authors)
-			);
+	const mergeBooks = (results, searchQueryString) => {
+		const match = new RegExp(escapeRegExp(searchQueryString), "i");
+		// find local results
+		const localResults = books.filter(
+			(b) => match.test(b.title) || match.test(b.authors)
+		);
 
-			const localBookIds = localResults.map((localBook) => localBook.id);
-			// remove local books from sever results
-			const serverResults = results.filter(
-				(serverBook) => !localBookIds.includes(serverBook.id)
-			);
+		const localBookIds = localResults.map((localBook) => localBook.id);
+		// remove local books from sever results
+		const serverResults = results.filter(
+			(serverBook) => !localBookIds.includes(serverBook.id)
+		);
 
-			// concat results and sort by title
-			const newShowingBooks = [...localResults, ...serverResults].sort(
-				(a, b) => b.title - a.title
-			);
-			// set new state
-			this.setState({ showingBooks: newShowingBooks });
-		};
+		// concat results and sort by title
+		const newShowingBooks = [...localResults, ...serverResults].sort(
+			(a, b) => b.title - a.title
+		);
+		// set new state
+		setShowingBooks(newShowingBooks);
+	};
 
-		const searchQuery = (searchQueryString) => {
-			BooksAPI.search(searchQueryString).then((results) => {
-				if (!results) {
-					this.setState({ showingBooks: [] });
-				} else {
-					mergeBooks(results, searchQueryString);
-				}
-			});
-		};
-
-		const updateQuery = (requestQuery) => {
-			this.setState({ query: requestQuery });
-			if (requestQuery) {
-				searchQuery(requestQuery);
+	const searchQuery = (searchQueryString) => {
+		BooksAPI.search(searchQueryString).then((results) => {
+			if (!results) {
+				setShowingBooks([]);
 			} else {
-				this.setState({ showingBooks: [] });
+				mergeBooks(results, searchQueryString);
 			}
-		};
+		});
+	};
 
-		const title = `Showing ${showingBooks.length} books`;
+	const updateQuery = (requestQuery) => {
+		const url = new URL(window.location);
 
-		return (
-			<div className="search-books">
-				<div className="search-books-bar">
-					<Link className="close-search" to="/">
-						Close
-					</Link>
-					<div className="search-books-input-wrapper">
-						<input
-							type="text"
-							placeholder="Search by title or author"
-							value={query}
-							onChange={(event) => updateQuery(event.target.value)}
-						/>
-					</div>
-				</div>
-				<div className="search-books-results">
-					<BookShelf
-						title={title}
-						books={showingBooks}
-						changeShelves={changeShelves}
+		setQuery(requestQuery);
+
+		if (requestQuery) {
+			window.history.pushState({ requestQuery }, "", `${url.origin}${url.pathname}?q=${requestQuery}`);
+			searchQuery(requestQuery);
+		} else {
+			window.history.pushState({ requestQuery }, "", `${url.origin}${url.pathname}`);
+			setShowingBooks([]);
+		}
+	};
+
+	window.onpopstate = (event) => {
+		const { requestQuery } = event.state;
+
+		setQuery(requestQuery);
+
+		if (requestQuery) {
+			searchQuery(requestQuery);
+		} else {
+			setShowingBooks([]);
+		}
+	};
+
+	useEffect(() => {
+		const urlParams = new URLSearchParams(window.location.search);
+
+		if (urlParams.has("q")) updateQuery(urlParams.get("q"));
+	}, []);
+
+	return (
+		<div className="search-books">
+			<div className="search-books-bar">
+				<Link className="close-search" to="/">
+					Close
+				</Link>
+				<div className="search-books-input-wrapper">
+					<input
+						type="text"
+						placeholder="Search by title or author"
+						value={query}
+						onChange={(event) => updateQuery(event.target.value)}
 					/>
 				</div>
 			</div>
-		);
-	}
-}
+			<div className="search-books-results">
+				<BookShelf
+					title={`Showing ${showingBooks.length} books`}
+					books={showingBooks}
+					changeShelves={changeShelves}
+				/>
+			</div>
+		</div>
+	);
+};
 
 Search.propTypes = {
 	changeShelves: PropTypes.func.isRequired,
